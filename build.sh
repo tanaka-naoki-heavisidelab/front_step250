554748f8bea19ebe27bc52939919127bdae08a4e
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-08-29 00:05:26 +0900

    success nodejs server2

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-cd server
+#!/bin/bash
+npm install http-server nodemon
+#以下スクリプトが無ければpackage.jsonに追記。
+#あればそのままマージされる。
+  # "scripts": {
+  #   "start": "http-server ./ -p 3000"
+  # }
 npm start
+# tail -f /dev/null
