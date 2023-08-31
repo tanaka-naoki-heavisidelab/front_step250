558d87ccf1f6efa50df2468a8e4e22ef6e08e161
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-08-31 16:09:09 +0900

    spa with ejs

--- server/public/main.js
+++ server/public/main.js
@@ -4,7 +4,7 @@ async function submitForm() {
   const password1 = document.getElementById('password1').value;
   const password2 = document.getElementById('password2').value;
 
-  const response = await fetch(baseUrl + '/fast/register/', {
+  const response = await fetch(baseUrl + '/fast/register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -16,12 +16,42 @@ async function submitForm() {
       password2: password2
     })
   });
+
   if (response.ok) {
     const data = await response.json();
-    console.log(data);
-    window.location.href = `/user?username=${data.username}&email=${data.email}`;
+
+    var template = `
+    <h2>登録情報</h2> 
+    <%= data.username %>
+    <%= data.email %>
+    `;
+
+    const html = ejs.render(template, { data: data });
+    console.info(html);
+    document.getElementById('output').innerHTML = html;
 
   } else {
     console.error('Error:', response.statusText);
   }
+
+  // if (response.ok) {
+  //   const data = await response.json();
+  //   console.log(data);
+
+  //   const messages = [data.username, data.email];
+  //   const template = document.getElementById('template').innerHTML;
+  //   const html = ejs.render(template, { messages });
+
+  //   document.getElementById('output').innerHTML = html;
+  // } else {
+  //   console.error('Error:', response.statusText);
+  // }
+  // if (response.ok) {
+  //   const data = await response.json();
+  //   console.log(data);
+  //   window.location.href = `/user?username=${data.username}&email=${data.email}`;
+
+  // } else {
+  //   console.error('Error:', response.statusText);
+  // }
 }
