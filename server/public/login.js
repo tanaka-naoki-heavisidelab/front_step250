a4df04652b40fbe3063b8fd8604409ddcae3f0af
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-09-01 23:46:06 +0900

    temp commit to change back oath2passwordform

--- server/public/login.js
+++ server/public/login.js
@@ -1,13 +1,13 @@
 async function submitForm() {
-  const email = document.getElementById('email').value;
+  const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
   const response = await fetch(baseUrl + '/fast/token', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: JSON.stringify({ email: email, password: password })
+    body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
   });
 
 
@@ -16,5 +16,27 @@ async function submitForm() {
     console.log(data)
   }
 
+  const messages = [];
+  if (response.ok) {
+    const data = await response.json();
+    messages.push("ログイン成功")
 
+    // Save the token in the Cookie    
+    // document.cookie = `userToken=${data.access_token}; Secure; HttpOnly; SameSite=Strict`;
+    document.cookie = `userToken=${data.access_token};`
+    // Include the token in the headers for subsequent requests
+    const headers = {
+      'Authorization': `Bearer ${data.access_token}`
+    };
+    window.location.href = '/home';
+  } else {
+    const errorData = await response.json();
+    messages.push(response.status)
+    messages.push(response.statusText)
+    messages.push(errorData.detail)
+    console.error(errorData.detail)
+  }
+
+  const html = ejs.render(templates.spaTemplate, { messages: messages });
+  document.getElementById('CLIENT_SIDE_RENDER').innerHTML = html;
 }
--- server/routes/home.js
+++ server/routes/home.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('home', { title: 'ホーム画面' });
+  res.render('home', { title: 'ホーム画面', user: "only / " });
+});
+
+/* GET home page. */
+router.get('/home', function (req, res, next) {
+  const userToken = req.cookies.userToken;
+
+
+  res.render('home', { title: 'ホーム画面', user: userToken });
 });
 
 module.exports = router;
