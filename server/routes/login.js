3dbfccf7c9f2388e2e573456e8dd162c6fe1158d
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-09-03 13:12:21 +0900

    divide ejs file in base and body

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,9 +1,29 @@
 var express = require('express');
+const fetch = require('node-fetch');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('login', { title: 'ログイン画面', baseUrl: process.env.BASE_URL });
+router.get('/', async function (req, res, next) {
+  const userToken = req.cookies.userToken;
+
+  // nginx:80…コンテナ間のSSRなのでlocalhostは使えない。
+  // backendとfrontendが共通のdocker networkで接続されている前提。
+  if (userToken) {
+    const response = await fetch('http://nginx:80/fast/user', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${userToken}`
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      res.render('login', { title: 'ログイン画面', baseUrl: process.env.BASE_URL, user: data.username });
+    } else {
+      res.render('login', { title: 'ログイン画面', baseUrl: process.env.BASE_URL, user: "error" });
+    }
+  } else {
+    res.render('login', { title: 'ログイン画面', baseUrl: process.env.BASE_URL, user: null });
+  }
 });
 
 module.exports = router;
