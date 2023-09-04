1f47a42dacb11b5e649fe312b1b6aa07cd5c4fd9
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-09-04 18:29:46 +0900

    add expire function

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,15 +8,14 @@ router.get('/', async function (req, res, next) {
   // nginx:80…コンテナ間のSSRなのでlocalhostは使えない。
   // backendとfrontendが共通のdocker networkで接続されている前提。
   if (userToken) {
-    const response = await fetch('http://nginx:80/fast/user', {
+    const response = await fetch('http://nginx:80/fast/usertasks', {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${userToken}`
       }
     });
 
-    const tasks = [];
-    tasks.push({ id: 1, title: "テストだよ", })
+    const today = new Date();
 
     if (response.ok) {
       const data = await response.json();
@@ -24,16 +23,18 @@ router.get('/', async function (req, res, next) {
         {
           title: 'タスク一覧画面',
           baseUrl: process.env.BASE_URL,
-          user: data.username,
-          tasks: tasks
+          today: today,
+          user: data.user,
+          tasks: data.tasks
         });
     } else {
       res.render('user',
         {
           title: 'タスク一覧画面',
           baseUrl: process.env.BASE_URL,
+          today: today,
           user: "error",
-          tasks: tasks
+          tasks: null
         });
     }
   } else {
@@ -41,6 +42,7 @@ router.get('/', async function (req, res, next) {
       {
         title: 'タスク一覧画面',
         baseUrl: process.env.BASE_URL,
+        today: today,
         user: null,
         tasks: null
       });
