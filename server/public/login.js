9c8f4f452c777f70816ed2b3bde8de4da332dbd6
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-09-06 10:42:32 +0900

    short and long token

--- server/public/login.js
+++ server/public/login.js
@@ -2,7 +2,7 @@ async function submitForm() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
-  const response = await fetch(baseUrl + '/fast/token', {
+  const response_1st = await fetch(baseUrl + '/fast/token', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
@@ -10,23 +10,44 @@ async function submitForm() {
     body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
   });
 
+  if (response_1st.ok) {
+    const data_1st = await response_1st.json();
+    console.log(data_1st)
+    const response_2nd = await fetch(`${baseUrl}/fast/token_refresh?refresh_token=${data_1st.access_token}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
 
-  if (response.ok) {
-    const data = await response.json();
-    console.log(data)
-    // Save the token in the Cookie    
-    // document.cookie = `userToken=${data.access_token}; Secure; HttpOnly; SameSite=Strict`;
-    document.cookie = `userToken=${data.access_token};`
-    // Include the token in the headers for subsequent requests
-    const headers = {
-      'Authorization': `Bearer ${data.access_token}`
-    };
-    window.location.href = '/home';
+    if (response_2nd.ok) {
+      const data_2nd = await response_2nd.json();
+      // Save the long-term token in the Cookie    
+      // HttpOnlyではoauth2_schemeが受け付けない。headerにAuthorization:を付与する必要があり
+      // JSで制御できねばならない。
+      // Secureを付けるとHTTPSのみになる。
+      // document.cookie = `userToken=${data_2nd.access_token}; HttpOnly; SameSite=Strict`;
+      document.cookie = `userToken=${data_2nd.access_token}`;
+      const headers = {
+        'Authorization': `Bearer ${data_2nd.access_token}`
+      };
+      window.location.href = '/home';
+    } else {
+      const messages = [];
+      const errorData = await response_2nd.json();
+      messages.push(response_2nd.status)
+      messages.push(response_2nd.statusText)
+      const errorDetail = errorData && errorData.detail ? errorData.detail : "An error occurred";
+      messages.push(errorDetail);
+      console.error(errorDetail);
+      const html = ejs.render(templates.spaTemplate, { messages: messages });
+      document.getElementById('CLIENT_SIDE_RENDER').innerHTML = html;
+    }
   } else {
     const messages = [];
-    const errorData = await response.json();
-    messages.push(response.status)
-    messages.push(response.statusText)
+    const errorData = await response_1st.json();
+    messages.push(response_1st.status)
+    messages.push(response_1st.statusText)
     messages.push(errorData.detail)
     console.error(errorData.detail)
     const html = ejs.render(templates.spaTemplate, { messages: messages });
