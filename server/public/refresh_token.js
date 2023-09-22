6971e37a8f321a139fe9d25c8e65db77a0483617
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-09-22 22:14:21 +0900

    update refresh logic

--- server/public/refresh_token.js
+++ server/public/refresh_token.js
@@ -1,5 +1,9 @@
 let refreshTimer;
-const INACTIVITY_DURATION = 10 * 60 * 1000; // 10 minutes, can be adjusted
+let inactivityTimer;
+const REFRESH_INTERVAL = 5 * 1000;
+const INACTIVITY_DURATION = 10 * 1000; // For testing
+// const REFRESH_INTERVAL = 10 * 60 * 1000; // 10 minutes
+// const INACTIVITY_DURATION = 30 * 60 * 1000; // 30 minutes
 
 async function refreshToken() {
   try {
@@ -19,18 +23,45 @@ async function refreshToken() {
   }
 }
 
+function startRefreshCycle() {
+  if (!refreshTimer) {
+    refreshTimer = setInterval(refreshToken, REFRESH_INTERVAL);
+  }
+}
+
 function handleActivity() {
-  // Reset the timer whenever the user does some activity
-  if (refreshTimer) {
-    clearTimeout(refreshTimer);
+  if (inactivityTimer) {
+    clearTimeout(inactivityTimer);
+    inactivityTimer = null;
   }
 
-  refreshTimer = setTimeout(() => {
-    refreshToken();
+  // Start the inactivity timer again
+  inactivityTimer = setTimeout(() => {
+    if (refreshTimer) {
+      clearInterval(refreshTimer);
+      refreshTimer = null;
+      console.log('Token refresh halted due to inactivity. Please re-login.');
+      // Here you can add logic to prompt the user to re-login or do other necessary actions.
+      fetch('/logout', {
+        method: 'GET',
+        credentials: 'include'  // Important to send and receive cookies
+      })
+        .then(response => {
+          if (response.ok) {
+            // Handle successful logout, e.g., redirect to login page
+            window.location.href = '/login';
+          } else {
+            // Handle error
+            console.error('Logout failed');
+          }
+        })
+        .catch(error => {
+          console.error('Error during logout:', error);
+        });
+    }
   }, INACTIVITY_DURATION);
 }
 
-// Check if the user is currently logged in
 async function checkUserStatus() {
   try {
     const response = await fetch(`${baseUrl}/fast/user`, {
@@ -42,17 +73,31 @@ async function checkUserStatus() {
     });
 
     if (response.ok) {
-      // If the user is logged in, start monitoring their activity
+      // If the user is logged in, start monitoring their activity and start the refresh cycle
       document.addEventListener('mousemove', handleActivity);
       document.addEventListener('keydown', handleActivity);
+      startRefreshCycle();
+      handleActivity();  // Start the inactivity timer
     } else {
-      // Otherwise, maybe handle the situation where the user isn't logged in
-      document.removeEventListener('mousemove', handleActivity);
-      document.removeEventListener('keydown', handleActivity);
+      // Otherwise, handle the situation where the user isn't logged in
+      stopRefreshAndListeners();
     }
   } catch (error) {
     console.error('Error checking user status:', error);
   }
+}
+
+function stopRefreshAndListeners() {
+  if (refreshTimer) {
+    clearInterval(refreshTimer);
+    refreshTimer = null;
+  }
+  if (inactivityTimer) {
+    clearTimeout(inactivityTimer);
+    inactivityTimer = null;
+  }
+  document.removeEventListener('mousemove', handleActivity);
+  document.removeEventListener('keydown', handleActivity);
 }
 
 // Start by checking the user's login status
